589bda0b11e6f28701fc095fadbc07ba09e7ed8d
mfathony115 <mfathony115@gmail.com>
2022-06-07 08:02:33 +0700

    configurasi database

--- routes/record.js
+++ routes/record.js
@@ -11,10 +11,10 @@ const multer = Multer({
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
-    host: 'public_ip_sql_instance_Anda',
+    host: '34.142.236.206',
     user: 'root',
-    database: 'nama_database_Anda',
-    password: 'password_sql_Anda'
+    database: 'submission2',
+    password: '1234567890'
 })
 
 router.get("/dashboard", (req, res) => {
